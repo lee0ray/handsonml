dfd35e7c3a49551599b24013e0bdf432f7d05ecd
lee <lee111ray@gmail.com>
2020-10-28 17:27:49 +0800

    1028

--- housing.py
+++ housing.py
@@ -2,12 +2,15 @@ import os
 import tarfile
 import urllib.request
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handsonml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 
-def fetch_housing_data(housing_url=HOUSING_URL,housing_path=HOUSING_PATH):
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     os.makedirs(housing_path, exist_ok=True)
     tgz_path = os.path.join(housing_path, "housing.tgz")
     urllib.request.urlretrieve(housing_url, tgz_path)
@@ -20,8 +23,32 @@ def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
-if __name__ == '__main__':
 
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+from zlib import crc32
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+
+from sklearn.model_selection import train_test_split
+
+if __name__ == '__main__':
     # fetch_housing_data()
     housing = load_housing_data()
     housing.head()
+    housing.hist(bins=50, figsize=(20, 15))
+    housing["latitude"].hist(bins=50, figsize=(20, 15))
+    plt.show()
+    train_set, test_set = train_test_split(housing, test_size=0.2,
+                                           random_state=42)
